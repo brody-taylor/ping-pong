c3aaeee65c61bbe5b6d6e12d31a66a44fa27238a
Brody Taylor <brody-taylor@users.noreply.github.com>
2022-05-03 21:42:18 -0400

    Event listeners for 'ping' command and message

--- index.js
+++ index.js
@@ -8,12 +8,38 @@ if (!token) {
 
 // Create a new client instance
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS]
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
 
 // Log once the client is ready
 client.once('ready', () => {
     console.log('Ready!');
+});
+
+const reply = 'Pong!'
+
+// Event listener for 'ping' command
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        console.log(`Received command: [${commandName}]`);
+        await interaction.reply(reply)
+            .then(() => console.log(`Reply sent: [${reply}]`))
+            .catch(console.error);
+    }
+});
+
+client.on('messageCreate', async msg => {
+    switch (msg.content.toLowerCase()) {
+        case "!ping":
+            console.log(`Received message: [${msg.content}]`);
+            await msg.channel.send(reply)
+                .then(msg => console.log(`Sent message: ${msg.content}`))
+                .catch(console.error);
+    }
 });
 
 // Login to Discord with your client's token
